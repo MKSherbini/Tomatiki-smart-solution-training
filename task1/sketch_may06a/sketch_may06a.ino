ccbf31212c260800e4afdedc2a01d06479591b38
Mohammad Khaled Abou El Sherbini <mh-sherbini@hotmail.com>
2019-05-06 19:11:52 +0200

    Update sketch_may06a.ino

--- task1/sketch_may06a/sketch_may06a.ino
+++ task1/sketch_may06a/sketch_may06a.ino
@@ -1,20 +1,21 @@
-unsigned long timer = 0;
+long timer = 0;
 void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
 
   if (millis() - timer > 2000) {
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(2, LOW);
     timer = millis();
   }
   else if (millis() - timer > 1000) {
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(2, HIGH);
   }
 
-  int sensorValue = analogRead(A0);
-  Serial.println(sensorValue);
-  delay(10);
+
+    int sensorValue = analogRead(A0);
+    Serial.println(sensorValue);
+    delay(10);
 }
